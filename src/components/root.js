b82d24ea4a68071a3b184a83ceba5a126c140a22
abhi12123 <vpofficial55@gmail.com>
2018-04-15 21:21:39 +0530

    set up redux

--- src/components/root.js
+++ src/components/root.js
@@ -2,16 +2,21 @@ import React, {Component} from 'react'
 import {init as firebaseInit} from 'javascripts/firebase'
 import {browserHistory} from 'react-router'
 import Routes from './routes'
+import {Provider} from 'react-redux'
+import configureStore from './configureStore'
 
 export default class Root extends Component {
   constructor(props) {
     super(props)
     firebaseInit()
+    this.store = configureStore()
   }
 
 render() {
-    return (
-      <Routes history={browserHistory}/>
+    return ( 
+      <Provider store={this.store}>
+        <Routes history={browserHistory}/>
+      </Provider>
     )
   }
 }
