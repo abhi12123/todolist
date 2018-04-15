a7a3b1e94b4fcf3a75e464f54739205815ec5642
abhi12123 <vpofficial55@gmail.com>
2018-04-15 22:02:46 +0530

    view created

--- src/javascripts/firebase.js
+++ src/javascripts/firebase.js
@@ -1,4 +1,7 @@
 import * as firebase from 'firebase'
+import sectionModel from './models/section'
+import todoModel from './models/todo'
+
 let database
 
 export const init = () => {
@@ -12,4 +15,32 @@ export const init = () => {
   }
   firebase.initializeApp(config)
   database = firebase.database()
+}
+
+export const getSectionsDB = () => {
+  return database.ref('/').once('value')
+}
+
+export const getTodoDB = (sectionId) => {
+  return database.ref(`/${sectionId}`).once('value')
+}
+
+export const addSection = (name) => {
+  let key = database.ref('/').push().key
+  let model = sectionModel(key, name, firebase.database.ServerValue.TIMESTAMP)
+  return database.ref('/'+ key).set(model)
+}
+
+// add new todo item into specified section
+export const addTodoItem = (id, name) => {
+  return new Promise((resolve, reject) => {
+    database.ref(`/${id}`).once('value').then((todo) => {
+      let todos = todo.val().todos || []
+      let key = database.ref(`/${id}`).push().key
+      todos.push(todoModel(key, name, firebase.database.ServerValue.TIMESTAMP))
+      database.ref(`/${id}/todos`).set(todos)
+        .then( res => {resolve(res)})
+        .catch( error => {reject(error)})
+    })
+  })
 }
